9b0dbef14d233bd84a6cf486ffdcc5f50977738b
Adhitya <adhityarpr00@gmail.com>
2020-07-03 19:04:02 +0700

    SplashScreen, Walkthrough

--- app/src/main/java/com/puck/rebyunime/SplashScreen.java
+++ app/src/main/java/com/puck/rebyunime/SplashScreen.java
@@ -3,6 +3,7 @@ package com.puck.rebyunime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -21,20 +22,22 @@ import android.widget.TextView;
     Selesai Mengerjakan Splash Screen
  */
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 5000;
+    private static int SPLASH_SCREEN = 1500;
 
     //Variables
     Animation topAnim, bottomAnim;
     ImageView image;
     TextView logo,info;
 
+    SharedPreferences onBoardingScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
 
         //Animations
         topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
@@ -49,13 +52,32 @@ public class MainActivity extends AppCompatActivity {
         logo.setAnimation(bottomAnim);
         info.setAnimation(bottomAnim);
 
-        /*new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Walkthrough.class);
-                startActivity(intent);
-                finish();
+
+                onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("FirstTime",true);
+
+                 if(isFirstTime){
+
+                     SharedPreferences.Editor editor = onBoardingScreen.edit();
+                     editor.putBoolean("FirstTime",false);
+                     editor.commit();
+
+                     Intent intent = new Intent(getApplicationContext(),OnBoarding.class);
+                     startActivity(intent);
+                     finish();
+
+                 }
+                 else{
+                     Intent intent = new Intent(getApplicationContext(),Home.class);
+                     startActivity(intent);
+                     finish();
+
+                 }
+
             }
-        },SPLASH_SCREEN); */
+        },SPLASH_SCREEN);
     }
 }
